7c4f155a0e43db52e0ebef5231461a50c6280706
Petrica Taras <petrica.taras@gmail.com>
2017-08-27 23:14:07 +0100

    2 more test functions added to diagnostics.h

--- Main_controller/diagnostics.h
+++ Main_controller/diagnostics.h
@@ -10,6 +10,7 @@
 /**
  * Function to generate (fake) sensor data for testing. It needs a mean value and 
  * a margin of variation 
+ * 
  * @param readings - the sensors array which needs data to be filled in
  * @param sizeReadings - the size of aforementioned readings array
  * @param meanValues - the mean value for fake sensor data
@@ -25,3 +26,40 @@ void simulateSensorDataWithRandomNoise(double readings[], int sizeReadings, doub
         readings[i] = ((double) random(randMin, randMax))/1000.0; 
     }
 }
+
+/**
+ * Function to generate slope variation sensor data for testing. It considers initial values,
+ * the amount of increase/decrease (sign dependent dx) and time difference (dt)
+ * A different approach would need to keep track of the history of readings[] array to update
+ * at the correct time. As it is, the control/decision is moved outside the function - i.e. the
+ * caller needs to ensure the readings[] progress in a nice sloped variation by repeatedly 
+ * calling the function with appropriate input values. Useful for testing functionality which
+ * triggers off after a certain threshold (say dropping under a certain temperature threshold)
+ * 
+ * @param readings - the sensors array which needs data to be filled in
+ * @param sizeReadings - the size of aforementioned readings array
+ * @param dx - the amount dx to be added
+ * @param dt - the time increment
+ */
+void simulateSensorDataWithSlopedVariation(double readings[], int sizeReadings, double dx[], double dt) {
+    for(int i = 0; i < sizeReadings; i++) {
+      readings[i] += (dx[i]/dt);
+    }
+}
+
+/**
+ * very basic diagnostic function (blinking the 13th pin LED) 
+ * 
+ * @param seconds the time in seconds it will blink
+ */
+
+ void blinkMe(int seconds) {
+     int increment = 1000;
+     int ledPin = 13;
+     for(int i = 0; i <= (seconds*1000); i+=increment) {
+         digitalWrite(ledPin, HIGH);
+         delay(increment/2);
+         digitalWrite(ledPin, LOW);
+         delay(increment/2);
+     }
+ }
