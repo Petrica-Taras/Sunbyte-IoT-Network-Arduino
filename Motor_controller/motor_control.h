d364296b00953c11eae6050a839c9dee4ff235a0
Petrica Taras <petrica.taras@gmail.com>
2017-09-24 18:33:31 +0100

    added tested code for the motors but did not properly merged them yet

--- Motor_controller/motor_control.h
+++ Motor_controller/motor_control.h
@@ -67,3 +67,72 @@ void twoAxisOpenLoopMotors(int steps[], boolean rDirection[], boolean mMode[], i
       delayMicroseconds(mDelay);      
   }  
 }  
+
+/**
+ * quick fix for github - needs to be merged properly
+ * into the rest of the code
+ */
+void sendToSerial() {
+  char* results = (char*) malloc(8*sizeof(char));
+  int localCounter = 0; // char level
+  int fullCounter = 0;  // char level
+  int countWords = 0;   // full word level
+
+ 
+  while(countWords < 6) {
+    char ch =  Serial.read(); 
+
+    if(ch == -1) continue; 
+    
+    //Serial.print(ch); 
+    
+        if((ch == ' ') || (countWords == 5)) {
+            results[localCounter] = '\0';       
+            localCounter=0; 
+
+            if(countWords == 0) {
+              steps[0] = atol(results);
+            } 
+            if(countWords == 1) {
+              steps[1] = atol(results); 
+            } 
+            if(countWords == 2) {
+              rDirection[0] = atoi(results); 
+            }
+            if(countWords == 3) {
+              rDirection[1] =  atoi(results); 
+            } 
+            if(countWords == 4) {
+              mMode[0] = atoi(results); 
+            }
+            if(countWords== 5) {
+                results[localCounter] = ch;
+                results[localCounter+1] = '\0';
+                mMode[1] = atoi(results); 
+                free(results);
+                fun(steps, rDirection, mMode);
+                break; 
+            } 
+            countWords++;
+            // Serial.println(countWords); 
+
+        }       
+        else {
+            results[localCounter]=ch; 
+            localCounter++; 
+            
+        }
+        
+        // Serial.print('a'); 
+  }
+  
+       shutDown(); 
+       countWords = 0;
+       localCounter = 0; 
+       steps[0] = 0; steps[1] = 0; 
+       rDirection[0] = 0; rDirection[1] = 0;
+       mMode[0] = 0; mMode[1] = 0;
+
+       // Serial.flush(); 
+
+}
